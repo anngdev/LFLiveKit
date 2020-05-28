bdad3c6afbfb26017d0e8a0b41b604a529302a76
An <anngdev@gmail.com>
2020-05-28 10:08:08 +0700

    Update LFLiveVideoConfiguration.m
    Auto rotation

--- LFLiveKit/configuration/LFLiveVideoConfiguration.m
+++ LFLiveKit/configuration/LFLiveVideoConfiguration.m
@@ -369,5 +369,16 @@
     [desc appendFormat:@" autorotate:%zi", self.autorotate];
     return desc;
 }
-
+- (void)refreshVideoSize {
+    CGSize size = self.videoSize;
+    if(self.landscape) {
+        CGFloat width = MAX(size.width, size.height);
+        CGFloat height = MIN(size.width, size.height);
+        self.videoSize = CGSizeMake(width, height);
+    } else {
+        CGFloat width = MIN(size.width, size.height);
+        CGFloat height = MAX(size.width, size.height);
+        self.videoSize = CGSizeMake(width, height);
+    }
+}
 @end
